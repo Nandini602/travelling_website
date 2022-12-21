2863bb4f8be95f801a09beb6e1b82e6112e6e69a
Nandini602 <107759330+Nandini602@users.noreply.github.com>
2022-12-21 15:03:10 +0530

    Add files via upload

---
+++ project/package.php
@@ -0,0 +1,293 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>package</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header">
+
+   <a href="home.php" class="logo">travel.</a>
+
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">package</a>
+      <a href="book.php">book</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<div class="heading" style="background:url(images/header-bg-2.png) no-repeat">
+   <h1>packages</h1>
+</div>
+
+<!-- packages section starts  -->
+
+<section class="packages">
+
+   <h1 class="heading-title">top destinations</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-1.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3> Taj Mahal and Agra City </h3>
+            <h4>1 Days </h4>
+            <p> This Tour offers you the rich heritage of Agra in 2 Days without any rush. The Tour includes sites like Taj Mahal, Agra Fort, Etimad-Ud-Daulah (Baby Taj), Mehtab Bagh (Sunset Point) and Fatehpur Sikri also known as Ghost City.</p>
+            <h3>₹ 7,999/- </h3>
+           
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-3.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>5 Days Luxurious Holiday To Bali</h3>
+            <h4>5 Days & 4 Nights </h4>
+            <p>Planning for a leisure trip for your upcoming holiday with your family? Thinking about spending some quality time with your beloved ones and create beautiful memories in this holiday that last forever?  .</p>
+       
+            <h3>₹ 34,999/- </h3> 
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-2.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Swanky vacation at splendid Krabi</h3>
+            <h4>4 Days & 3 Nights </h4>
+            <p>Famed for its breathtaking views, enchanting beaches and islands while sitting on the shores of the Andaman Sea, and crammed with a plenty of offerings, Krabi town is here to leave you stunned at every visit. </p>
+            <h3>₹ 12,999/- </h3>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-4.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Amazing Thailand Tour Package</h3>
+            <h4> 7 Days & 6 Nights </h4>
+            <p>Thailand, the land of beaches, royal palaces, tranquil seas and ancient ruins; is a Southeast Asian country and a major tourist destination. Bangkok, Phuket, Krabi and Pattaya are some of t all kinds of tourists visit it.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-5.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Breathtaking Bangkok Tour Package</h3>
+            <h4> 5 Days & 4 Nights </h4>
+            <p>People come here to let go off the stress and relax in the lap of nature. Therefore, booking a 4 nights 5 days Bangkok tour package seems the best way to avoid any last minute rush in deciding the itineraries.</p>
+            <h3>₹19,499/- </h3>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-6.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Mauritius Family Vacation: Wildlife & Beaches</h3>
+            <h4> 5 Days & 4 Nights </h4>
+            <p>Explore the sparkling beaches and revel amidst the exotic flora & fauna with customizable 4 nights 5 days Mauritius family packages from India.</p>
+            <h3>₹37,699/-</h3>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-7.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Stunning Phuket Honeymoon Package</h3>
+            <h4> 4 Days & 3 Nights </h4>
+            <p>Your dream honeymoon comes true with this 3 night 4 days honeymoon package, including all the honeymoon places in Phuket. Kindle the flame of love in the rainforested, mountainous island in the Andaman Sea</p>
+            <h3>₹ 27,000/</h3>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-8.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Memorable Mauritius Tour Package</h3>
+            <h4> 4 Days & 3 Nights </h4>
+            <p>Mauritius is an island nation located off the southeast coast of the African continent in the Indian Ocean. It is a beautiful island of lagoons and palm-lined beaches with coral reefs surrounding most of the coastline. </p>
+            <h3>₹ 35,000/</h3>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-9.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>A Blissful Romantic Tour To Andaman Island</h3>
+            <h4> 4 Days & 3 Nights </h4>
+            <p>Hold your beloved hand and stroll around the seashore. Enjoy the blissful atmosphere. Enjoy the sightseeing with your better half. Visit North Bay Island would be an exhilarating experience</p>
+            <h3> ₹ 15,600/-</h3>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-10.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Spectacular Dubai Sightseeing Tour Packages</h3>
+            <h4> 7 Days &  6 Nights </h4>
+            <p> Create memories with your loved ones with our affordable 6 nights, 7 days Dubai Sightseeing Packages. Dubai, a dazzling city, with the world's tallest building dwelling right in here seeks your visit today</p>
+            <h3> ₹ 46,600/-</h3>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-11.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3> 5 Days Tour Package To Vietnam With Airfare</h3>
+            <h4> 5 Days &  4 Nights </h4>
+            <p> You can see many history along with cultures in this place. If you want to visit this then go for 4 nights 5 days Vietnam Tour packages from TravelTriangle</p>
+            <h3> ₹ 44,899/-</h3>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-12.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3> Extravagant Port Blair Sightseeing Tour Packages</h3>
+            <h4> 4 Days &  3 Nights </h4>
+           <p>Port Blair, on South Andaman Island, is the capital city of the Andaman and Nicobar Islands, an Indian territory in the Bay of Bengal. Its seafront Cellular Jail, completed in 1906, hints at its past as a British penal colony . </p>
+           <h3> ₹12,600/-</h3>
+           <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+   </div>
+
+   <div class="load-more"><span class="btn">load more</span></div>
+
+</section>
+
+<!-- packages section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+         <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +123-456-7890 </a>
+         <a href="#"> <i class="fas fa-phone"></i> +111-222-3333 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> shaikhanas@gmail.com </a>
+         <a href="#"> <i class="fas fa-map"></i> mumbai, india - 400104 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <div class="credit"> created by <span>mr. web designer</span> | all rights reserved! </div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
